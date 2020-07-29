640fa0ad5164acc9f89712ab30d3915f97ae6e81
Himanshu <himanshu@symboinsurance.com>
2020-07-29 14:52:40 +0530

    Pallindrome Test

--- src/main/java/com/sai/programs/PallindromString.java
+++ src/main/java/com/sai/programs/PallindromString.java
@@ -18,7 +18,7 @@ public class PallindromString {
         int end = s.length() - 1;
 
         int i = 0;
-        while (begin < end) {
+        while (begin < end) {                               // Check if inward search intersected
             System.out.println(++i);
             if (s.charAt(begin) != s.charAt(end)) return false;
             begin++;
@@ -28,7 +28,7 @@ public class PallindromString {
     }
 
     private static boolean isPalindromeBruteForceOutward(String s) {
-        int left =( s.length() / 2 + s.length() % 2 )- 1;
+        int left =( s.length() / 2 + s.length() % 2 )- 1;   //To get left outward begin point
         int right = s.length() % 2 == 0 ? left + 1 : left;
         int i = 0;
         while (left != -1) {
